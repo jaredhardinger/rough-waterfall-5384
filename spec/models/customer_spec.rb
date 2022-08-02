7d1e6a9d990318935bf7317fae40bfc6a97a39f1
Jared Hardinger <jaredhardinger@gmail.com>
2022-08-02 17:20:54 -0400

    Add model test for customer total price

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe Customer, type: :model do
     it { should have_many :customer_items } 
     it { should have_many :items} 
   end
+
+  describe 'class methods' do
+    it 'has a total price' do
+      kroagur = Supermarket.create!(name: 'Kroagur', location: 'Ohio')
+      kingsloops = Supermarket.create!(name: 'King Sloopers', location: 'Colorado')
+      
+      item1a = Item.create!(name: 'cheese', price: 3)
+      item1b = Item.create!(name: 'onion', price: 2)
+      item1c = Item.create!(name: 'pepper', price: 1)
+      item2a = Item.create!(name: 'rice', price: 4)
+
+      steve = kroagur.customers.create!(name: 'Steve')
+      sally = kingsloops.customers.create!(name: 'Sally')
+
+      CustomerItem.create!(customer_id: steve.id, item_id: item1a.id)
+      CustomerItem.create!(customer_id: steve.id, item_id: item1b.id)
+      CustomerItem.create!(customer_id: steve.id, item_id: item1c.id)
+      CustomerItem.create!(customer_id: sally.id, item_id: item2a.id)
+
+      expect(steve.total_price).to eq(6)
+    end
+  end
 end
